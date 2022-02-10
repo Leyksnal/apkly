c0b6137baa9c446c6d62041dd4c0c3b89e82a79e
leyksnal <leyksnal@gmail.com>
2022-02-10 16:16:24 +0100

    hero and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="hero">
+        <div className="header-wrapper">
+          <div className="header">
+            <div className="logo">
+              <p>Apkly.</p>
+            </div>
+            <div className="nav">
+              <span>Home</span>
+              <span>Overview</span>
+              <span>Feature</span>
+              <span>Price</span>
+              <span>Contact</span>
+              <button className='header-btn'>Sign Up</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="hero-content">
+          <div className="hero-text">
+            <h2>SpaceHunt</h2>
+            <h3>Startup Of The Year</h3>
+            <p>You may proceed to get the latest information based on what technology our company is improving or innovating</p>
+            <button className='header-btn hero-btn'>Updates</button>
+          </div>
+          <div className="hero-img-section">
+            <div className="xmark"></div>
+            <div className="hero-img"></div>
+            <div className="circle"></div>
+          </div>
+        </div>
+
+        <div className="triangle"></div>
+      </div>
     </div>
   );
 }
